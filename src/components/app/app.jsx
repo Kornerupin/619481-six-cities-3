64e47115de74f1d1c4a0e6957aac8363a94f762d
Braylovskiy Andrey <kornerupin@yandex.ru>
2020-03-03 21:12:22 +0300

    Add PropTypes checking - validation

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Main from "../main/main";
+import PropTypes from "prop-types";
 
 const App = (props) => {
   return <Main
-    // eslint-disable-next-line react/prop-types
     placesCount={props.placesCount}
+    titles={props.titles}
   />;
+};
+
+App.PropTypes = {
+  placesCount: PropTypes.integer.isRequired,
+  titles: PropTypes.array.isRequired
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const rootNode = document.querySelector(`#root`);
+const titles = [
+  `Beautiful & luxurious apartment at great location`,
+  `Wood and stone place`,
+  `No name`,
+  `Very big title by the best hotel`
+];
 
 ReactDOM.render(
     <App
       placesCount={329}
+      titles={titles}
     />,
     rootNode
 );
